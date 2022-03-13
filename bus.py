f46afd1603a672675cc07104d0c23f09d735e396
James Cavallo <jcavallo@colgate.edu>
2022-03-13 19:12:47 +0100

    Code?

--- bus.py
+++ bus.py
@@ -4,4 +4,74 @@
 #
 #######################################################
 
+def main():
+    departureTime = input("Please enter a departure time in HH:MM ")
+    distance = float(input("How far do you need to travel in Km? "))
+    stops = input("How many stops are there along the way? ")
+    newHours = 0
+    newMinutes = 0
+
+    departureList = departureTime.split(":")
+    hours = departureList[0]
+    minutes = departureList[1]
+
+    if hours[0] == "0":
+        hours = hours[1]
+    if minutes[0] == "0":
+        minutes = minutes[1]
+
+    hours = int(hours)
+    minutes = int(minutes)
+
+    travelTime = ((distance / 40.0) * 3600) + (int(stops) * 30) #time in seconds to travel
+    travelHours = travelTime // 3600 #hours to travel
+    travelTime = round(travelTime - (travelHours * 3600)) #account for some weird 
+    travelMinutes =  travelTime // 60 #minutes of travel
+    travelSeconds = round(travelTime - (travelMinutes * 60)) #seconds of travel
+
+
+    if (minutes + travelMinutes) > 60:
+        if int(((minutes + travelMinutes) // 60) + travelHours + hours) > 24:
+            newHours = int(((minutes + travelMinutes) // 60) + travelHours + hours) - (newHours % 24)
+            newMinutes =  int((minutes + travelMinutes) % 60)
+            if newMinutes == 60:
+                newMinutes = 0
+                newHours+=1
+        elif int(((minutes + travelMinutes) // 60) + travelHours + hours) == 24:
+             newHours = 0
+             newMinutes =  int((minutes + travelMinutes) % 60)
+        else:
+            newHours = int(((minutes + travelMinutes) // 60) + travelHours + hours)
+            newMinutes =  int((minutes + travelMinutes) % 60)
+            if newMinutes == 60:
+                newMinutes = 0
+                newHours+=1
+    else:
+        newMinutes = int((minutes + travelMinutes))
+        newHours = int(travelHours + hours)
+        if newMinutes == 60:
+                newMinutes = 0
+                newHours+=1
+        if newHours > 24:
+            newHours = newHours % 24
+        elif newHours == 24:
+            newHours = 0
+
+    if newHours < 10:
+        newHours = "0" + str(newHours)
+    if newMinutes < 10:
+        newMinutes = "0" + str(newMinutes)
+    travelSeconds = int(travelSeconds)
+    if travelSeconds == 0:
+        travelSeconds = "00"
+    
+    print("Arrival time: " + str(newHours) + ":" + str(newMinutes) + ":" + str(travelSeconds))
+    
+
+
+    
+
+if __name__ == "__main__":
+    main()
+
 
--- face.py
+++ face.py
@@ -4,5 +4,251 @@
 #
 #######################################################
 
-import turtle
+import turtle 
 
+
+def main():
+    ninja = turtle.Turtle()
+    s = ninja.getscreen()
+    s.title("The Cardiff Wizard")
+    s.setworldcoordinates(0, 0, 400, 400)
+    s.bgcolor("royal blue")
+    ninja.speed(100)
+
+    ninja.pencolor("green")
+    ninja.forward(40)
+
+    ninja.pencolor("black")
+    ninja.fillcolor("grey")
+    ninja.begin_fill()
+
+    ninja.left(90)
+    ninja.forward(20)
+
+    for i in range(11):
+        ninja.right(90)
+        ninja.forward(40)
+        ninja.right(90)
+        ninja.forward(20)
+        ninja.right(90)
+        ninja.forward(40)
+        ninja.right(90)
+        ninja.forward(40)
+    ninja.end_fill()
+
+    ninja.begin_fill()
+    ninja.forward(10)
+    ninja.right(90)
+    ninja.forward(15)
+    ninja.right(90)
+    ninja.forward(30)
+    ninja.right(90)
+    ninja.forward(10)
+    ninja.left(180)
+    ninja.forward(10)
+    ninja.end_fill()
+    ninja.forward(30)
+
+    ninja.pencolor("brown")
+    ninja.forward(120)
+    ninja.left(90)
+
+    ninja.fillcolor("gold")
+    ninja.begin_fill()
+    ninja.pencolor("gold")
+    for i in range(2):
+        ninja.forward(125)
+        ninja.right(90)
+        ninja.forward(5)
+        ninja.right(90)
+    ninja.forward(125)
+    ninja.left(90)
+    ninja.end_fill()
+
+    ninja.fillcolor("red")
+    ninja.pencolor("green")
+
+    ninja.begin_fill()
+    ninja.forward(40)
+    ninja.left(90)
+    ninja.forward(20)
+    ninja.left(90)
+    ninja.forward(40)
+    ninja.left(90)
+    ninja.forward(20) 
+    ninja.end_fill()
+
+    ninja.penup()
+    ninja.right(90)
+    ninja.forward(5)
+    ninja.right(90)
+    ninja.forward(125)
+    ninja.left(90)
+    ninja.pendown()
+
+    ninja.pencolor("brown")
+    ninja.forward(120)
+
+
+
+    ninja.fillcolor("brown")
+    ninja.pencolor("brown")
+    ninja.begin_fill()
+    for i in range(2):
+        ninja.right(90)
+        ninja.forward(15)
+        ninja.right(90)
+        ninja.forward(250)
+    ninja.end_fill()
+    ninja.fillcolor("grey")
+    ninja.pencolor("black")
+    for i in range(11):
+        ninja.begin_fill()
+        ninja.forward(40)
+        ninja.right(90)
+        ninja.forward(20)
+        ninja.right(90)
+        ninja.forward(40)
+        ninja.right(90)
+        ninja.forward(20)
+        ninja.end_fill()
+        ninja.right(180)
+        ninja.forward(20)
+        ninja.left(90)
+        ninja.end_fill()
+    ninja.fillcolor("saddle brown")
+    ninja.begin_fill()
+    ninja.left(180)
+    ninja.forward(250)
+    ninja.right(90)
+    ninja.forward(205)
+    ninja.right(90)
+    ninja.forward(250)
+    ninja.right(90)
+    ninja.forward(205)
+    ninja.end_fill()
+
+
+    ninja.right(180)
+    ninja.forward(220)
+    ninja.right(90)
+    ninja.forward(25)
+    ninja.pencolor("black")
+    ninja.fillcolor("grey")
+    ninja.begin_fill()
+    ninja.left(90)
+    ninja.forward(30)
+    ninja.right(90)
+    ninja.forward(15)
+    ninja.right(90)
+    ninja.forward(30)
+    ninja.right(90)
+    ninja.forward(15)
+    ninja.end_fill()
+    ninja.forward(300)
+    ninja.right(90)
+    ninja.begin_fill()
+    ninja.forward(30)
+    ninja.left(90)
+    ninja.forward(15)
+    ninja.left(90)
+    ninja.forward(30)
+    ninja.left(90)
+    ninja.forward(15)
+    ninja.end_fill()
+    ninja.forward(155)
+    ninja.right(90)
+    ninja.penup()
+    ninja.forward(220)
+    ninja.pendown()
+
+
+
+    ninja.right(270)
+    colors = ["red", "blue", "green", "gray", "orange", "black"]
+
+    for i in range(70, 0, -4):
+        ninja.fillcolor(colors[i%6])
+        ninja.begin_fill()
+        ninja.circle(i)
+        ninja.end_fill()
+
+    ninja.penup()
+    ninja.left(90)
+    ninja.forward(70)
+    ninja.pendown()
+
+    ninja.right(90)
+    ninja.fillcolor("tan")
+    ninja.begin_fill()
+    ninja.circle(10)
+    ninja.end_fill()
+
+    ninja.penup()
+    ninja.left(90)
+    ninja.forward(20)
+    ninja.pendown()
+
+    ninja.fillcolor("purple")
+    ninja.begin_fill()
+    ninja.right(90)
+    ninja.forward(10)
+    ninja.left(120)
+    ninja.forward(20)
+    ninja.left(120)
+    ninja.forward(20)
+    ninja.end_fill()
+    ninja.left(120)
+    ninja.forward(10)
+    ninja.right(90)
+
+    ninja.penup()
+    ninja.forward(10)
+    ninja.left(90)
+
+    ninja.forward(3)
+
+    ninja.pendown()
+    ninja.forward(2)
+    ninja.penup()
+
+
+
+    ninja.left(180)
+    ninja.forward(8)
+
+    ninja.pendown()
+    ninja.forward(2)
+    ninja.penup()
+
+    ninja.right(180)
+    ninja.forward(5)
+    ninja.right(90)
+
+    ninja.forward(5)
+    ninja.pendown()
+
+    ninja.left(90)
+    ninja.forward(5)
+    ninja.left(180)
+    ninja.forward(10)
+    ninja.right(180)
+    ninja.forward(5)
+    ninja.right(90)
+
+    ninja.penup()
+    ninja.forward(5)
+    ninja.pendown()
+    ninja.begin_fill()
+    ninja.right(35)
+    ninja.forward(50)
+    ninja.left(125)
+    ninja.forward(60)
+    ninja.left(130)
+    ninja.forward(55)
+    ninja.end_fill()
+    turtle.done()
+
+
+if __name__ == "__main__":
+    main()
